9676aedb266cd80cc9988857f402cbcc90738ac8
hankyeol <lhn030703@gmail.com>
2024-06-04 06:00:25 +0900

    -main에서 pull 한 코드병합 후 수정 -gui : admin 구현

--- banking.py
+++ banking.py
@@ -6,11 +6,16 @@ from tkinter import messagebox
 random.seed()
 
 #sqlite3 DB
-conn = sqlite3.connect('card.s3db')#이전에 명명된 db와 연결
+conn = sqlite3.connect('card.s3db') #이전에 명명된 db와 연결
 cur = conn.cursor() #cursor object 생성으로 sql script 동작
 # cur.execute("DROP TABLE card")
 # card 테이블이 없는 경우 생성
-cur.execute("CREATE TABLE IF NOT EXISTS card(id INTEGER PRIMARY KEY,number TEXT,pin TEXT,balance INTEGER DEFAULT 0);")
+cur.execute("""
+CREATE TABLE IF NOT EXISTS card(
+    id INTEGER PRIMARY KEY,
+    number TEXT,
+    pin TEXT,balance INTEGER DEFAULT 0
+    );""")
 conn.commit() #DB에 결과 저장
 
 
@@ -28,11 +33,78 @@ class Card:
         self.row = []
         self.balance = 0
         self.receiver_balance = 0
-        
+        self.language = 'en'
+        self.messages = {
+            'en': {
+                'welcome': "\n1. Create an account\n2. Log into account\n9. Switch Language\n0. Exit",
+                'menu': "\n1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n9. Switch Language\n0. Exit",
+                'create_card': "Your card has been created\nYour card number:",
+                'create_pin': "Your card PIN:",
+                'enter_card': "Enter your card number:",
+                'enter_pin': "Enter your PIN:",
+                'login_success': "\nYou have successfully logged in",
+                'login_failure': "Wrong card number or PIN",
+                'balance': '\nBalance: ',
+                'enter_income': '\nEnter income:',
+                'income_added': 'Income was added!',
+                'transfer': '\nTransfer\nEnter card number:',
+                'mistake_card_number': 'Probably you made a mistake in the card number. Please try again!',
+                'no_card': 'Such a card does not exist.',
+                'enter_transfer_amount': "Enter how much money you want to transfer:\n",
+                'not_enough_money': "Not enough money!",
+                'transfer_success': "Success!",
+                'account_closed': '\nThe account has been closed!',
+                'logout_success': "\nYou have successfully logg out!",
+                'bye': "\nBye!",
+                'invalid_input': "Invalid input",
+            },
+            'ko': {
+                'welcome': "\n1. 계좌 생성\n2. 로그인\n9. 영한 변환\n0. 종료",
+                'menu': "\n1. 잔액 확인\n2. 수입 추가\n3. 이체\n4. 계좌 폐쇄\n5. 로그아웃\n9. 영한 변환\n0. 종료",
+                'create_card': "카드가 생성되었습니다\n카드 번호:",
+                'create_pin': "카드 PIN:",
+                'enter_card': "카드 번호를 입력하세요:",
+                'enter_pin': "PIN을 입력하세요:",
+                'login_success': "\n성공적으로 로그인했습니다",
+                'login_failure': "잘못된 카드 번호 또는 PIN",
+                'balance': '\n잔액: ',
+                'enter_income': '\n수입을 입력하세요:',
+                'income_added': '수입이 추가되었습니다!',
+                'transfer': '\n이체\n카드 번호를 입력하세요:',
+                'mistake_card_number': '카드 번호에 실수가 있는 것 같습니다. 다시 시도하세요!',
+                'no_card': '존재하지 않는 카드입니다.',
+                'enter_transfer_amount': "이체할 금액을 입력하세요:\n",
+                'not_enough_money': "잔액이 부족합니다!",
+                'transfer_success': "성공!",
+                'account_closed': '\n계좌가 폐쇄되었습니다!',
+                'logout_success': "\n성공적으로 로그아웃했습니다!",
+                'bye': "\n안녕!",
+                'invalid_input': "잘못된 입력",
+            }
+        }
         
         # 실행부분을 init으로 옮겨야 함. main에서 실행할 수 없음.
         self.main_window()
         
+       
+    def translate(self, message):
+        return self.messages[self.language][message]
+    #en기본언어 ko변경 감지시 switch
+    def switch_language(self):
+        self.language = 'ko' if self.language == 'en' else 'en'
+    
+        
+    def create_account(self): #카드번호 생성
+        print(self.translate('create_card'))
+        # 랜덤한 카드 번호 생성
+        self.card = '400000' + str(random.randint(100000000, 999999999)) 
+        print(self.luhn()) #luhn을 통해 검증가능한 유효 카드번호 반환
+        print(self.translate('create_pin'))
+        self.pin = str(random.randint(1000, 9999)) #랜덤한 pin번호 생성
+        print(self.pin)
+        # 생성된 계정 정보를 데이터베이스에 삽입
+        cur.execute("INSERT INTO card (number, pin) VALUES (?, ?);", (self.card, self.pin)) #DB에 카드, pin 정보 추가
+        conn.commit() # 저장    
         
     def destroy_window(self): # gui 에서 화면을 지우는 코드. 처음 화면 초기화 시에 사용
         for kidwin in self.root.winfo_children():
@@ -55,11 +127,12 @@ class Card:
         button1 = tk.Button(self.root, text="1. Create account", command=self.create_account, width=20, height=10)
         button2 = tk.Button(self.root, text="2. Log in", command=self.log_in, width=20, height=10)
         button3 = tk.Button(self.root, text="3. Exit", command=self.exit, width=20, height=10)
+        button4 = tk.Button(self.root, text="Admin", command=self.admin_menu, width=5, height=2)
 
         button1.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
         button2.grid(row=1, column=1, padx=10, pady=10, sticky='nsew')
         button3.grid(row=1, column=2, padx=10, pady=10, sticky='nsew')
-
+        button4.grid(row=0, column=2, padx=10, pady=10, sticky='nsew')
 
         # 그리드 행과 열 구성 조정
         self.root.grid_rowconfigure(3, weight=1)
@@ -91,8 +164,8 @@ class Card:
         cur.execute(f"INSERT INTO card (number, pin) VALUES (?, ?)", (self.card, self.pin)) # DB에 카드, pin 정보 추가
         conn.commit() # 저장
         
-    def log_in(self): #카드번호와 PIN을 받아서 sql문으로 검색, 검색된 경우 sucess문으로 넘어감
-        
+    def log_in(self): #카드번호와 PIN을 받아서 sql문으로 검색, 검색된 경우 sucess문으로 넘어감 
+        self.login_attempts = 0  # 로그인 시도 횟수 초기화
         #self.login_card = input("Enter your card number:\n")
         #self.login_pin = input("Enter your PIN:\n")
         login_win = tk.Toplevel(self.root)
@@ -136,9 +209,52 @@ class Card:
 
         else:
             #print("wrong card number or pin")
-            messagebox.showerror("error","wrong card number or pin")
+            self.login_attempts += 1
+            if (self.login_attempts >= 5): #로그인 실패 횟수가 5번이 넘어가면 종료, 악의적 로그인 시도의 차단.
+                messagebox.showerror("error","Too many failed login attempts. program will exit.")
+                sys.exit()
+            messagebox.showerror("error",f"wrong card number or pin. {5-self.login_attempts} attempts left.")
         '''elif not self.luhn_2(self.login_card):
                 print('Probably you made a mistake in the card number. Please try again!') '''
+        
+
+    
+    '''    
+    def log_in(self): #카드번호와 PIN을 받아서 sql문으로 검색, 검색된 경우 sucess문으로 넘어감
+
+        login_attempts = 0  # 로그인 시도 횟수 초기화
+    
+        while login_attempts < 5:
+            self.login_card = input("Enter your card number:\n")
+            self.login_pin = input("Enter your PIN:\n")
+        
+            cur.execute("""SELECT
+                            id,
+                            number,
+                            pin,
+                            balance
+                        FROM 
+                            card
+                        WHERE
+                            number = ? AND pin = ?;""", (self.login_card, self.login_pin)) #입력받은 정보를 통해 sql문으로 db조회
+
+            self.row = cur.fetchone() #만약 cur에 입력받은게 있으면 그 값을 가져오고, 없다면 NULL을 가져옴
+            if self.row: #계정 존재시 로그인 성공
+                self.balance = self.row[3] #db의 네번째 요소인 balance(id, number, pin, balance 중 balance)를 이 instance의 balance에 넣음.
+                print('\nYou have successfully logged in')
+                self.success() #로그인 성공시 메뉴
+                return
+
+            else:
+                login_attempts += 1 
+                print("Wrong card number or PIN")
+                if login_attempts >= 5: #로그인 실패 횟수가 5번이 넘어가면 종료, 악의적 로그인 시도의 차단.
+                    print("Too many failed login attempts. program will exit.")
+                    conn.close()
+                    sys.exit() 
+        '''
+
+                            
 
                 
     def success(self): #1.자산확인 2.입금기능 3.송금기능 4.계좌폐쇄 5.로그아웃 0.종료
@@ -307,10 +423,135 @@ class Card:
         self.card = (''.join(map(str, lst2)))
         
         return self.card
+
+
+    def admin_menu(self): #관리자 기능 메소드 1.전체 계좌 목록 조회 2.특정 계좌 삭제 0.관리자 기능 종료
         
+        self.admin_password = 1000 #비밀번호는 1000
+        adminlogin = tk.Toplevel(root)
+        adminlogin.title("Admin Login")
+        adminlogin.geometry("300x100")
+        
+        adminlogin.grab_set()
+        
+        tk.Label(adminlogin, text="Enter Admin Password:").pack(pady=5)
+        input_password = tk.Entry(adminlogin, width=20)
+        input_password.pack(pady=5)
+        tk.Button(adminlogin, width = 5,text="Enter", command=lambda:self.check_admin(adminlogin, input_password)).pack(pady=10)
+        
+        #input_password = int(input("password: "))
+        
+    def check_admin(self,adminlogin, input_password):
+        try:
+            input_password = int(input_password.get())
+            if self.admin_password == input_password:
+                adminwin = tk.Tk()
+                adminwin.title("Admin Menu")
+                adminwin.geometry("200x300")
+                adminwin.resizable(width=False, height=False)
+                #admin 기능 버튼 추가
+                tk.Button(adminwin,text="View all accounts",command=lambda:self.showallacounts(adminwin)).grid(row=5,column=10,padx = 40,pady=20)
+                tk.Button(adminwin,text="Delete an account",command=lambda:self.deleteaccountcheck(adminwin)).grid(row=10,column=10,padx = 40,pady=20)
+                tk.Button(adminwin,text="Back to main menu",command=lambda:self.adminwin.destroy).grid(row=15,column=10,padx = 40,pady=20)
+
+                adminlogin.destroy()
+            else:
+                messagebox.showerror("Error","Wrong password")
+        except ValueError:
+            messagebox.showerror("Error","Invalid integer input")
+        
+    def showallacounts(self,adminwin):
+        #전체 계좌 목록 조회. 
+        
+        #새 창 열기
+        allaccounts = tk.Toplevel(adminwin)
+        allaccounts.title("All Accounts")
+        allaccounts.geometry("800x500")
+        
+        cur.execute("SELECT id, number, pin, balance FROM card;")
+        accounts = cur.fetchall()
+        #print("\nAll Accounts:")
+        for account in accounts:
+            tk.Label(allaccounts,text=f"ID: {account[0]}, Number: {account[1]}, PIN: {account[2]}, Balance: {account[3]}").pack(pady=5)
+            #print(f"ID: {account[0]}, Number: {account[1]}, PIN: {account[2]}, Balance: {account[3]}")
     
+    def deleteaccountcheck(self,adminwin):
+        #특정 계정 삭제 
+        delaccwin = tk.Toplevel(adminwin)
+        delaccwin.title("Delete Account")
+        delaccwin.geometry("300x100")
+        #d  elaccwin.grab_set()
+        
+        tk.Label(delaccwin, text="Enter card number to delete:").pack(pady=5)
+        delaccount = tk.Entry(delaccwin, width=20)
+        delaccount.pack(pady=5)
+        tk.Button(delaccwin, text="Delete", command=lambda:self.deleteaccount(delaccount,delaccwin),width=5).pack(pady=10)
+    def deleteaccount(self,delaccount,delaccwin):
+        try:
+            delete_card = delaccount.get()
+            cur.execute("SELECT * FROM card WHERE number = ?;",(delete_card,))
+            if cur.fetchone():
+                delaccwin.destroy()
+                cur.execute("DELETE FROM card WHERE number = ?;",(delete_card,))
+                conn.commit()
+                messagebox.showinfo("info","The account has been deleted!")
+            else:
+                print("Such a card does not exist.")
+        except ValueError:
+            messagebox.showerror("Error",self.translate('invalid_input'))
+     
+            '''     while (admin_password == input_password): 
+            print("""\nAdmin Menu
+1. View all accounts
+2. Delete an account
+0. Back to main menu""")
+            i = int(input())
+            if i == 1:  # 전체 계좌 목록 조회
+                cur.execute("SELECT id, number, pin, balance FROM card;")
+                accounts = cur.fetchall()
+                print("\nAll Accounts:")
+                for account in accounts:
+                    print(f"ID: {account[0]}, Number: {account[1]}, PIN: {account[2]}, Balance: {account[3]}")
+            elif i == 2:  # 특정 계정 삭제
+                print('\nEnter card number to delete:')
+                delete_card = input()
+                cur.execute("SELECT * FROM card WHERE number = ?;",(delete_card))
+                if cur.fetchone():
+                    cur.execute("DELETE FROM card WHERE number = ?;",(delete_card))
+                    conn.commit()
+                    print("The account has been deleted!")
+                else:
+                    print("Such a card does not exist.")
+            elif i == 0: # 관리자 기능 종료
+                break
+            else:
+                print(self.translate('invalid_input'))'''
+       
 
 
+    def menu(self): #메뉴 표시 메소드 1.계좌생성 2.로그인 9.영한변환 0.종료
+        while True:
+            print(self.translate('welcome'))
+
+            i = int(input())
+            if i == 1:
+                self.create_account()
+            elif i == 2:
+                self.log_in()
+
+            elif i == 3:
+                self.admin_menu()
+
+            elif i == 9:
+                self.switch_language()
+
+            elif i == 0:
+                conn.close()
+                print(self.translate('bye'))
+                break
+            else:
+                print(self.translate('invalid_input'))
+                
 
 #instance 생성 후 menu 메소드 실행
 
