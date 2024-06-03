b2870339f621c3eeff60a56bd0d21484ab806e92
KimChaeWon12 <kcw030220@naver.com>
2024-06-03 13:00:35 +0900

    코드 유지보수를 위한 주석처리 및 입출금 및 이체 등 db기록처리 개선

--- banking.py
+++ banking.py
@@ -2,12 +2,14 @@
 import random
 import sys
 import sqlite3
+
+#랜덤 번호 생성을 위한 시드 설정
 random.seed()
 
-#sqlite3 DB
+#sqlite3 DB 연결
 conn = sqlite3.connect('card.s3db')#이전에 명명된 db와 연결
-cur = conn.cursor() #cursor object 생성으로 sql script 동작
-# cur.execute("DROP TABLE card")
+cur = conn.cursor() #SQL 스크랩트를 실행하기 위한 커서 객체 생성
+
 # card 테이블이 없는 경우 생성
 cur.execute("CREATE TABLE IF NOT EXISTS card(id INTEGER PRIMARY KEY,number TEXT,pin TEXT,balance INTEGER DEFAULT 0);")
 conn.commit() #DB에 결과 저장
@@ -15,7 +17,7 @@ conn.commit() #DB에 결과 저장
 
 class Card:
 
-    def __init__(self): #생성자 card의 instance attribute : card, pin, login_card, login_pin, row, balance, receiver_balance 값 초기화
+    def __init__(self): #인스턴스 속성 초기화
         self.card = ''
         self.pin = ''
         self.login_card = ''
@@ -24,25 +26,30 @@ class Card:
         self.balance = 0
         self.receiver_balance = 0
         
-    def create_account(self): #카드번호 생성
+    def create_account(self): #새 계정 생성 후 카드 번호와 핀 생성
         print("Your card has been created")
         print("Your card number:")
+
         # 랜덤한 카드 번호 생성
         self.card = '400000' + str(random.randint(100000000, 999999999)) 
-        print(self.luhn()) #luhn을 통해 검증가능한 유효 카드번호 반환
+        print(self.luhn()) #luhn 알고리즘을 통해 검증가능한 유효 카드번호 반환
+
+        #랜덤한 핀 생성
         print("Your card PIN:")
         self.pin = str(random.randint(1000, 9999)) #랜덤한 pin번호 생성
         print(self.pin)
-        # 생성된 계정 정보를 데이터베이스에 삽입
-        cur.execute(f"""INSERT INTO card (number, pin) VALUES ({self.card}, {self.pin});""") #DB에 카드, pin 정보 추가
-        conn.commit() # 저장
-        
-    def log_in(self): #카드번호와 PIN을 받아서 sql문으로 검색, 검색된 경우 sucess문으로 넘어감
 
+        # 새 계정 정보를 데이터베이스에 삽입
+        cur.execute("INSERT INTO card (number, pin) VALUES (?, ?);", (self.card, self.pin)) #DB에 카드, pin 정보 추가
+        conn.commit() #변경 사항 저장
+        
+    def log_in(self):
+        #카드 번호와 핀을 사용하여 기존 계정에 로그인
         self.login_card = input("Enter your card number:\n")
         self.login_pin = input("Enter your PIN:\n")
 
-        cur.execute(f"""SELECT
+        # 입력한 카드 번호와 핀으로 데이터베이스에서 조회
+        cur.execute("""SELECT
                             id,
                             number,
                             pin,
@@ -50,15 +57,13 @@ class Card:
                         FROM 
                             card
                         WHERE
-                            number = {self.login_card}
-                            AND pin = {self.login_pin}
-                        ;""") #입력받은 정보를 통해 sql문으로 db조회
+                            number = ? AND pin = ?;""", (self.login_card, self.login_pin)) #입력받은 정보를 통해 sql문으로 db조회
 
-        self.row = cur.fetchone() #만약 cur에 입력받은게 있으면 그 값을 가져오고, 없다면 NULL을 가져옴
-        if self.row: #계정 존재시 로그인 성공
+        self.row = cur.fetchone() #조회 결과를 가져옴
+        if self.row: #계정이 존재할 경우
             self.balance = self.row[3] #db의 네번째 요소인 balance(id, number, pin, balance 중 balance)를 이 instance의 balance에 넣음.
             print('\nYou have successfully logged in')
-            self.success() #로그인 성공시 메뉴
+            self.success() #계정 메뉴로 이동
 
         else:
             print("wrong card number or pin")
@@ -75,7 +80,7 @@ class Card:
 5. Log out
 0. Exit""")
             i = int(input())
-            if i == 1: #자산확인
+            if i == 1: #잔액 확인
                 print('\nBalance: ', self.balance) #현재 내 자산 출력
                 print()
             elif i == 2: #입금기능
@@ -83,35 +88,39 @@ class Card:
                 amount = int(input())
                 self.balance += amount #기존잔고 + 입금액
                 # 잔액 업데이트 및 입금 내역 데이터베이스에 기록
-                cur.execute(f'UPDATE card SET balance = {self.balance} WHERE number = {self.login_card};')
-                conn.commit() #DB저장
+                cur.execute("UPDATE card SET balance = ? WHERE number = ?;", (self.balance, self.login_card))
+                conn.commit() #변경 사항 저장
                 print('Income was added!')
-            elif i == 3: #송금기능
+            elif i == 3: #이체기능
                 print('\nTransfer\nEnter card number:')
                 receiver_card = input() #돈을 보낼 카드번호를 입력받고 db에 receiver_card에 저장.
                 cur.execute(f'SELECT id, number,pin,balance FROM card WHERE number = {receiver_card};')
 
                 if not self.luhn_2(receiver_card): #룬2에서 반환받은 값이 false라면
                     print('Probably you made a mistake in the card number. Please try again!')
-                elif not cur.fetchone(): #카드번호 자체가 없을시
-                    print('Such a card does not exist.')
-                else:
-                    transfer = int(input("Enter how much money you want to transfer:\n")) #송금액 입력
-                    if transfer > self.balance:
-                        print("Not enough money!") #송금액이 자산총액보다 많을 경우
-                    else: #계좌에서 실제로 이체
-                        self.balance -= transfer #송금자 자산총액에서 송금액 제외. DB에 업데이트
-                        cur.execute(f'UPDATE card SET balance = {self.balance} WHERE number = {self.login_card};')
-                        self.receiver_balance += transfer #수금자 자산총액에서 송금액 추가. DB에 업데이트
-                        cur.execute(f'UPDATE card SET balance = {self.receiver_balance} WHERE number = {receiver_card};')
-                        cur.execute(f'SELECT * FROM card WHERE number = {self.login_card}') #송금자 카드번호를 기반으로 DB내 모든 정보 조회
-                        print(cur.fetchone())
-                        print("Success!")
-                        conn.commit()
 
-            elif i == 4: #계좌폐쇄
-                cur.execute(f"DELETE FROM card WHERE number = {self.login_card}") #sql문으로 해당 카드와 관련된 DB내 모든 정보 삭제
-                conn.commit()
+                else:
+                    #데이터베이스에서 수신자의 카드 조회
+                    cur.execute("SELECT id, number, pin, balance FROM card WHERE number = ?;", (receiver_card,))
+                    receiver_row = cur.fetchone()
+                    if not receiver_row:
+                        print('Such a card does not exist.')
+                    else:
+                        transfer = int(input("Enter how much money you want to transfer:\n")) #송금액 입력
+                        if transfer > self.balance:
+                            print("Not enough money!") #잔액이 부족할 경우
+                        else: #계좌에서 실제로 이체
+                            self.balance -= transfer #송금자의 잔액에서 이체 금액 차감
+                            cur.execute("UPDATE card SET balance = ? WHERE number = ?;", (self.balance, self.login_card))
+
+                            #수신자의 잔액 업데이트
+                            self.receiver_balance = receiver_row[3] + transfer #수금자 자산총액에서 송금액 추가. DB에 업데이트
+                            cur.execute("UPDATE card SET balance = ? WHERE number = ?;", (self.receiver_balance, receiver_card))
+                            conn.commit() #변경 사항 저장
+                            print("Success!")
+            elif i == 4: #계좌 해지
+                cur.execute("DELETE FROM card WHERE number = ?", (self.login_card,)) #sql문으로 해당 카드와 관련된 DB내 모든 정보 삭제
+                conn.commit() #변경 사항 저장
                 print('\nThe account has been closed!')
                 break
             elif i == 5: #로그아웃
@@ -119,11 +128,11 @@ class Card:
                 break
             elif i == 0: #종료
                 print("\nBye!")
-                conn.close()
+                conn.close() #데이터베이스 연결 종료
                 sys.exit()
 
     # 룬2 : 카드번호의 유효성. 이 검사를 통과할 시 true, 못하면 false
-    def luhn_2(self, num): #Luhn 알고리즘을 사용해 카드번호가 유효한지 판단
+    def luhn_2(self, num): #Luhn 알고리즘을 사용해 카드번호가 유효성 검사
         num2 = num[:]
         num2 = num2[::-1]
         lst = [int(x) for x in num2]
@@ -141,7 +150,7 @@ class Card:
         return False
                 
     # Defining my luhn algorithm for creating a new card number
-    def luhn(self): #Luhn 알고리즘을 사용해 새 카드번호를 정의하는 메소드
+    def luhn(self): #Luhn 알고리즘을 사용해 새 카드 번호 생성
 
         lst2 = [int(x) for x in self.card]
         lst3 = lst2[:]
